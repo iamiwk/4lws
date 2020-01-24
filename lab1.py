f3e85a3318b252077cf5b25cb553ff6d89b958a3
iamiwk <31501733+iamiwk@users.noreply.github.com>
2020-01-24 15:25:43 +0300

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,18 @@
+
+print ('arrays')
+m = int (input ())
+print ('increase')
+c = int (input ())
+a = int ()
+b = int ()
+i = 0
+array = []
+while i < m: 
+   
+    print ('int array')
+    a = int (input ()) * c
+    b = int (input ()) * c
+    array.append(a)
+    array.append(b)
+    i = i+1
+print (array)
