8ce132291b1b941f99f52c0729a12573d373c271
iamiwk <31501733+iamiwk@users.noreply.github.com>
2020-01-24 20:51:05 +0300

    Create teslab.py

---
+++ teslab.py
@@ -0,0 +1,15 @@
+class part1:
+    print ('how much numbers in array?')
+    N = int (input ())
+    print ('multiply by')
+    c = int (input ())
+    a = int ()
+    i = 0
+    array = []
+    print ('int array', N,'numbers')
+    while i < N: 
+        a = int (input ()) * c
+        array.append(a)
+        i = i+1
+
+print (part1.array)
