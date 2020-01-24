dad38b5b7945be77a3f417dd0ccfe982753db370
iamiwk <31501733+iamiwk@users.noreply.github.com>
2020-01-24 20:51:22 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,18 +1,23 @@
 
-print ('arrays')
-m = int (input ())
-print ('increase')
-c = int (input ())
-a = int ()
-b = int ()
-i = 0
-array = []
-while i < m: 
+import threading
+class part1:
+    print ('how much numbers in array?')
+    n = int (input ())
+    print ('multiply by')
+    c = int (input ())
+    a = int ()
+    b = int ()
+    i = 0
+    array = []
+    while i < n: 
    
-    print ('int array')
-    a = int (input ()) * c
-    b = int (input ()) * c
-    array.append(a)
-    array.append(b)
-    i = i+1
+        print ('int array')
+        a = int (input ()) * c
+        b = int (input ()) * c
+        array.append(a)
+        array.append(b)
+        i = i+1
 print (array)
+
+class part2:
+thread.
