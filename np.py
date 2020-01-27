e62c79c0f6c0dbf4662132de46cf482847d9ecff
iamiwk <31501733+iamiwk@users.noreply.github.com>
2020-01-27 15:20:36 +0300

    Create np.py

---
+++ np.py
@@ -0,0 +1,17 @@
+import numpy as np 
+
+class part1:
+    print ('how much numbers in array?')
+    N = int (input ())
+    print ('multiply by')
+    c = int (input ())
+    a = int ()
+    i = 0
+    array = np.array ([])
+    print ('int array', N,'numbers')
+    while i < N: 
+        a = int (input ()) * c
+        array = np.append(array,a)
+        i = i+1
+
+print (part1.array)
